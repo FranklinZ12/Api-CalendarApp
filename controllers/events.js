d24f1bb0455633b8bebf042f3da2982ffd221832
FranklinZ12 <franklinq8@gmail.com>
2022-03-29 10:41:44 -0500

    fucionalidad actualizar y eliminar evento completada

--- controllers/events.js
+++ controllers/events.js
@@ -1,10 +1,13 @@
 import { response } from "express";
 import { ModeloEvento } from "../models/Evento.js";
 
-const getEventos = (req, res = response) => {
+const getEventos = async (req, res = response) => {
+    const eventos = await ModeloEvento.find()
+        .populate('user', 'name')
+
     res.json({
         ok: true,
-        msg: 'getEventos'
+        eventos
     })
 }
 
@@ -27,18 +30,80 @@ const crearEvento = async (req, res = response) => {
     }
 }
 
-const actualizarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'actualizarEvento'
-    })
+const actualizarEvento = async (req, res = response) => {
+
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const evento = await ModeloEvento.findById(eventoId);
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no encontrado por el id'
+            });
+        };
+
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No autorizado para editar eventos'
+            });
+        };
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        };
+
+        const eventoActualizado = await ModeloEvento.findByIdAndUpdate(eventoId, nuevoEvento, { new: true });
+
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado en ActualizarEventos'
+        })
+    }
 }
 
-const eliminarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'eliminarEvento'
-    })
+const eliminarEvento = async (req, res = response) => {
+
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const evento = await ModeloEvento.findById(eventoId);
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no encontrado por el id'
+            });
+        };
+
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No autorizado para eliminar eventos'
+            });
+        };
+
+        await ModeloEvento.findByIdAndDelete(eventoId);
+
+        res.json({
+            ok: true,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado en EliminarEventos'
+        })
+    }
 }
 
 export { getEventos, crearEvento, actualizarEvento, eliminarEvento };
